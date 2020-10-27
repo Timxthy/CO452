20a644ed40882ecf7b1942b526711b36b66e811f
Timxthy <Timxthy@Github.com>
2020-10-27 12:00:41 +0000

    Added course class

---
+++ app01-lab-classes/Course.java
@@ -0,0 +1,26 @@
+
+/**
+ * A class called Course which contains a code number and a title.
+ *
+ * @author (Timothy Arole)
+ * @version (27.10.2020)
+ */
+public class Course
+{
+    
+  
+    //This is the course code
+    private String code;
+    //This is the course title
+    private String title;
+    
+    public Course (String coursecode, String coursetitle)
+    {
+        code = coursecode;
+        title = coursetitle;
+    
+   
+        
+    }
+    
+}
